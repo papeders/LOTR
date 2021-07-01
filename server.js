371fd654da8676c98ba20eecd53813797e7032c3
Pachel A Pederson <oreb197@upcorp.ad.uprr.com>
2021-07-01 09:49:15 -0500

    backend packages installed

---
+++ server.js
@@ -0,0 +1,24 @@
+//imports
+const express = require('express');
+const app = express ();
+const cors = require('cors');
+
+const corsOptions = {
+    origin: ['http://localhost:3000'],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true, //allows session cookies to be sent back and forth
+    optionsSuccessStatus: 200 //legacy browsers
+}
+
+app.use(cors(corsOptions))
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:false})) //apply this to all app requests. gives us req.body
+
+app.use(bodyParser.json());
+
+
+// run server
+app.listen(3000, () => {
+    console.log("I am listening!")
+})
