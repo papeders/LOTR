1dfb177327adcdfeffb2f6a5838a3a1f66e94276
Pachel A Pederson <oreb197@upcorp.ad.uprr.com>
2021-07-01 09:55:16 -0500

    planning and constants

---
+++ constants.js
@@ -0,0 +1,9 @@
+// error messages
+module.exports = {
+    INTERNAL_SERVER_ERROR : 500,
+    SUCCESS: 200,
+    UNAUTHORIZED: 401,
+    BAD_REQUEST: 400,
+    FORBIDDEN: 403,
+    BEARER_START_INDEX: 7
+}
