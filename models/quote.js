ea5d50c979bdc03b7eb213c5c8d643be649e1943
Pachel A Pederson <oreb197@upcorp.ad.uprr.com>
2021-07-01 16:56:05 -0500

    models associations made

--- models/quote.js
+++ models/quote.js
@@ -4,13 +4,8 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Quote extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Quote.belongsTo(models.User, {foreignKey: 'userID'})
     }
   };
   Quote.init({
