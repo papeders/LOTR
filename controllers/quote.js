444dfd193f3651110aaa5d21e6592dba3858f5d4
Pachel A Pederson <oreb197@upcorp.ad.uprr.com>
2021-07-02 10:56:16 -0500

    quote models and routes

--- controllers/quote.js
+++ controllers/quote.js
@@ -2,3 +2,66 @@ const Quote = require('../models').Quote
 const User = require('../models').User
 
 const constants = require('../constants')
+
+const getQuotes = (req, res) => {
+    User.findOne({
+        where: {username: req.params.username}
+    }).then(resp => {
+
+        if (resp != null){
+            const newQuote = {
+                quoteID: req.body.quoteID,
+                char: req.body.char,
+                userID: resp.id
+            }
+
+            Quote.findAll({
+                where: {userID: resp.id}
+            }).then((resp) => {
+                res.send(resp)
+            })
+        } else {
+            res.status(constants.BAD_REQUEST).send('Error: Username not found.')
+        }
+    }).catch(err => {
+        res.status(constants.BAD_REQUEST).send(`Error: ${err}`)
+    })
+}
+
+const addQuote = (req, res) => {
+    User.findOne({
+        where:{username: req.params.username}
+    }).then( resp => {
+
+        if (resp != null) {
+            const newQuote = {
+                quoteID: req.body.quoteID,
+                char: req.body.char,
+                userID: resp.id
+            }
+
+            Drink.create(newQuote)
+                .then( resp => {
+                    res.status(constants.SUCCESS).json(resp)
+                })
+        } else {
+            res.status(constants.BAD_REQUEST).send('Error: Username not found.')
+        }
+    }).catch( err => {
+        res.status(constants.BAD_REQUEST).send(`Error: ${err}`)
+    })
+}
+
+const deleteQuote = (req, res) => {
+    Quote.destroy({where: {id: req.params.id}})
+        .then( resp => {
+            res.status(constants.SUCCESS).send('Quote deleted.')
+        })
+}
+
+module.exports = {
+  
+    getQuotes,
+    addQuote,
+    deleteQuote
+}
--- routes/quote.js
+++ routes/quote.js
@@ -2,5 +2,8 @@ const express = require('express')
 const router = express.Router()
 const ctrl = require('../controllers')
 
+router.get('/:username', ctrl.quote.getQuotes)
+router.post('/:username', ctrl.quote.addQuote)
+router.delete('/:id', ctrl.quote.deleteQuote)
 
 module.exports = router
