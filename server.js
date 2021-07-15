60599191e1e084a6ec553d264dc06cce7615cd99
Pachel A Pederson <oreb197@upcorp.ad.uprr.com>
2021-07-15 17:28:27 -0500

    cleaned up code

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//imports
 const express = require('express');
 const app = express ();
 const cors = require('cors');
@@ -6,24 +5,22 @@ const cors = require('cors');
 const corsOptions = {
     origin: ['http://localhost:3000'],
     methods: "GET,POST,PUT,DELETE",
-    credentials: true, //allows session cookies to be sent back and forth
-    optionsSuccessStatus: 200 //legacy browsers
+    credentials: true, 
+    optionsSuccessStatus: 200 
 }
 
 app.use(cors(corsOptions))
 
 const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended:false})) //apply this to all app requests. gives us req.body
+app.use(bodyParser.urlencoded({extended:false})) 
 
 app.use(bodyParser.json());
 
 const routes = require('./routes')
 
-//routes
 app.use('/quote', routes.quote) 
 app.use('/user', routes.user)
 
-// run server
 app.listen(3001, () => {
     console.log("I am listening!")
 })
