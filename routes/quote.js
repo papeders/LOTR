e9c4d59542c5dbaa7fc67ec693a49fb43ee8069a
Pachel A Pederson <oreb197@upcorp.ad.uprr.com>
2021-07-15 17:25:20 -0500

    removed add and delete quotes

--- routes/quote.js
+++ routes/quote.js
@@ -3,7 +3,5 @@ const router = express.Router()
 const ctrl = require('../controllers')
 
 router.get('/:username', ctrl.quote.getQuotes)
-router.post('/:username', ctrl.quote.addQuote)
-router.delete('/:id', ctrl.quote.deleteQuote)
 
 module.exports = router
